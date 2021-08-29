646a74d57933c121bf1c34a72f55ee25955517ac
Hepheir-ZenBook <hepheir@gmail.com>
2021-08-29 15:23:42 +0900

    client: Refactor SplendorCoinPile

--- client/src/components/SplendorCoinPile/index.jsx
+++ client/src/components/SplendorCoinPile/index.jsx
@@ -1,19 +1,22 @@
+import classNames from "classnames";
 import React from "react";
 
 import "./style.css";
 
+import SplendorCoin from "../SplendorCoin";
+
 class SplendorCoinPile extends React.Component {
     render() {
-        const { gem_type, count } = this.props;
+        const { gem_type, count, className } = this.props;
         const stack = [];
 
         for (let i = 0; i < count; i++) {
             const unique_key = `spl_coin-pile_${gem_type}_${i}`;
-            stack.push(<div className="spl_coin-pile" key={unique_key} />);
+            stack.push(<SplendorCoin className="spl_coin-pile_coin" gem_type={gem_type} key={unique_key} />);
         }
 
         return (
-            <div className="spl_coin-pile_container" data-gem-type={gem_type}>
+            <div className={classNames("spl_coin-pile", className)}>
                 {stack}
             </div>
         );
