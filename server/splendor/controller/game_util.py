51263be622809db999af9263cbd1cf1cac3d73a5
Hepheir <hepheir@gmail.com>
2021-09-04 15:00:29 +0900

    server/controller: Create game_util.py

---
+++ server/splendor/controller/game_util.py
@@ -0,0 +1,36 @@
+from splendor.database.splendor_card import SplendorCard
+from splendor.database.splendor_tile import SplendorTile
+from splendor.model.components import Card, Cost, Tile, Game, Player
+
+
+def round(game: Game) -> int:
+    return game.game_turn_number // len(game.game_players) + 1
+
+
+def current_player(game: Game) -> Player:
+    return game.game_players[game.game_turn_number % len(game.game_players)]
+
+
+def cast_card(db_card: SplendorCard) -> Card:
+    return Card(
+        card_id=db_card.card_id,
+        card_level=db_card.card_level,
+        card_score=db_card.card_score,
+        card_bonus=db_card.card_bonus,
+        card_illustration=db_card.card_illustration,
+        card_cost=Cost(diamond=db_card.card_cost_diamond,
+                       sapphire=db_card.card_cost_sapphire,
+                       emerald=db_card.card_cost_emerald,
+                       ruby=db_card.card_cost_ruby,
+                       onyx=db_card.card_cost_onyx))
+
+
+def cast_tile(db_tile: SplendorTile) -> Tile:
+    return Tile(tile_id=db_tile.tile_id,
+                tile_score=db_tile.tile_score,
+                tile_illustration=db_tile.tile_illustration,
+                tile_cost=Cost(diamond=db_tile.tile_cost_diamond,
+                               sapphire=db_tile.tile_cost_sapphire,
+                               emerald=db_tile.tile_cost_emerald,
+                               ruby=db_tile.tile_cost_ruby,
+                               onyx=db_tile.tile_cost_onyx))
