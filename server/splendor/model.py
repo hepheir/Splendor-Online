4602325239276312a2f3feccadbb88a68f7a1aed
Hepheir-ZenBook <hepheir@gmail.com>
2021-09-05 20:37:26 +0900

    server/model: Create User model

--- server/splendor/model.py
+++ server/splendor/model.py
@@ -1,5 +1,7 @@
 from enum import IntEnum, auto
 
+from splendor import database
+
 
 class COMPONENT_TYPE(IntEnum):
     CARD = auto()
@@ -26,3 +28,32 @@ class GAME_STATE(IntEnum):
     END_TURN = auto()
     WAITING_FOR_PRE_ACTION = auto()
     WAITING_FOR_POST_ACTION = auto()
+
+
+class User:
+    def __init__(self, name: str) -> None:
+        print(f'[SYSTEM] Creating user with name "{name}"...')
+        self._user_name = name
+        if self.db_row:
+            print(f'[ERROR] User with name: "{name}" already exists.')
+            print()
+            return
+        database.insert_one('user', value={"user_name": self.user_name})
+        print(f'[SYSTEM] Successfully created {self}.')
+        print()
+
+    def __repr__(self) -> str:
+        return f'<user_name:{self.user_name}>'
+
+    def __del__(self) -> None:
+        database.delete_all('user', query={"user_name": self.user_name})
+        print(f'[SYSTEM] User {self} has expired.')
+        print()
+
+    @property
+    def user_name(self) -> str:
+        return self._user_name
+
+    @property
+    def db_row(self):
+        return database.select_one('user', query={"user_name": self.user_name})
