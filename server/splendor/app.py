b0033a6272b75f32ef650bd5cefa80f471e50376
Hepheir-ZenBook <hepheir@gmail.com>
2021-09-15 16:17:42 +0900

    server: Add database using `flask-sqlalchemy`

--- server/splendor/app.py
+++ server/splendor/app.py
@@ -5,13 +5,22 @@ import typing as t
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "tmp_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+
 
 CORS(app)
+db = SQLAlchemy(app)
 socketio = SocketIO(app)
+
+
+@app.teardown_request  # type: ignore
+def shutdown_session(exception=None):
+    db.session.remove()
 
 
 def run(
@@ -22,6 +31,7 @@ def run(
     **options: t.Any,
 ) -> None:
     """Runs the application on a local development server."""
+    db.create_all()
     socketio.run(
         app, host, port, debug=debug, load_dotenv=load_dotenv, **options
     )
