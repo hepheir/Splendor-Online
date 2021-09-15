b65c1fef362fcca81c88e76a08a860e62d29efb5
Hepheir-ZenBook <hepheir@gmail.com>
2021-09-15 10:09:57 +0900

    server/app: Use `flask-socketio` and `flask-cors` for `app`

--- server/splendor/app.py
+++ server/splendor/app.py
@@ -1,4 +1,15 @@
-import flask
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask_cors import CORS
+from flask_socketio import SocketIO
 
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "tmp_secret_key"
+
+CORS(app)
+socketio = SocketIO(app)
+
+
+run = socketio.run
