3c19332824f57a0cf38166be8c1da6dfadb79b8b
Hepheir-ZenBook <hepheir@gmail.com>
2021-09-15 10:31:25 +0900

    server/app: Create function `run()`

--- server/splendor/app.py
+++ server/splendor/app.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import typing as t
+
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
@@ -12,4 +14,14 @@ CORS(app)
 socketio = SocketIO(app)
 
 
-run = socketio.run
+def run(
+    host: t.Optional[str] = None,
+    port: t.Optional[int] = None,
+    debug: t.Optional[bool] = None,
+    load_dotenv: bool = True,
+    **options: t.Any,
+) -> None:
+    """Runs the application on a local development server."""
+    socketio.run(
+        app, host, port, debug=debug, load_dotenv=load_dotenv, **options
+    )
